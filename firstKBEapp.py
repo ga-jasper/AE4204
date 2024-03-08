cf18a956c33a940db6d6faa02f098dfcd4c0177d
Jasper <jasper.john.palmer@gmail.com>
2024-03-08 15:51:46 +0100

    first (real-ish) commit!

---
+++ firstKBEapp.py
@@ -0,0 +1,26 @@
+class Aircraft():
+    v_min = Input(100)
+    v_max = Input(400)
+    @Attribute
+    def weight(self):
+        return (self.fuselageWeight + self.tailWeight + self.wingWeight + self.propWeight)
+class Wing():
+    @Part
+    def slat(self):
+        return Slat()
+    def flap(self):
+        return Flap()
+class HLD(Aircraft):
+    v_min = Aircraft.v_min
+    v_max = Aircraft.v_max
+    @Part
+    def actuator(self):
+        return Actuator()
+class Actuator():
+    powerRequired = 0
+
+class Flap(HLD):
+    span = Input()
+    chord = Input()
+
+class Slat(HLD):
